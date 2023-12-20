a0d978c7e372b07955c818d30ed8b8e1bade7a15
Andrew Capaldo <andycapaldo2@gmail.com>
2023-12-20 14:29:13 -0700

    Minor change to navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,18 +14,18 @@ type HeaderProps = {
 
 export default function Header({ loggedInUser, isLoggedIn, handleLogOut }: HeaderProps) {
     return (
-        <Navbar expand="lg" className="my-nav">
+        <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand as={Link} to='/home'> <img src={logo} className="mainNavLogo"/>The Cocktail Lounge</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/cocktails">User Creations</Nav.Link>
-                    <Nav.Link as={Link} to={`/profile/${loggedInUser?.id}`}>Profile</Nav.Link>
+                <Navbar.Brand as={Link} to='/home' className="navText"> <img src={logo} className="mainNavLogo"/>{' '}The Cocktail Lounge</Navbar.Brand>
+                <Nav className="d-flex justify-content-center">
+                    <Nav.Link className="navText" as={Link} to="/cocktails">User Creations</Nav.Link>
+                    <Nav.Link className="navText" as={Link} to={`/profile/${loggedInUser?.id}`}>Profile</Nav.Link>
                     { isLoggedIn ? (
-                        <Nav.Link as={Link} to='/' onClick={handleLogOut}>Logout</Nav.Link>
+                        <Nav.Link className="navText" as={Link} to='/' onClick={handleLogOut}>Logout</Nav.Link>
                     ) : (
                         <>
-                            <Nav.Link as={Link} to="/login">Log In</Nav.Link>
-                            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+                            <Nav.Link className="navText" as={Link} to="/login">Log In</Nav.Link>
+                            <Nav.Link className="navText" as={Link} to="/signup">Sign Up</Nav.Link>
                         </>
                     )}
                 </Nav>
